df23d05c166915b86f86d7f1fff6ba4cbc733530
Lucas Fernandes <fernandes20lucas@outlook.com>
2021-10-11 15:32:56 -0300

    Cria o cadastro do Seller

--- SalesSystem/Controllers/SellersController.cs
+++ SalesSystem/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesSystem.Models;
 using SalesSystem.Services;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,16 @@ namespace SalesSystem.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
--- SalesSystem/Services/SellerService.cs
+++ SalesSystem/Services/SellerService.cs
@@ -14,5 +14,9 @@ namespace SalesSystem.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj) {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
