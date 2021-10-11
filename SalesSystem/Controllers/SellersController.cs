ae7bec7bcd1a7c2ee1e7fadb9428951f0bdd14d0
Lucas Fernandes <fernandes20lucas@outlook.com>
2021-10-11 17:04:45 -0300

    Adiciona a pagina Details para o Seller

--- SalesSystem/Controllers/SellersController.cs
+++ SalesSystem/Controllers/SellersController.cs
@@ -47,5 +47,16 @@ namespace SalesSystem.Controllers {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Details(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
