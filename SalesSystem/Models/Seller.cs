877aafbfb433a4ab1cf372350c6691c7aaf93e2b
Lucas Fernandes <fernandes20lucas@outlook.com>
2021-11-20 17:06:31 -0300

    realiza validação dos campos ao criar um seller

--- SalesSystem/Models/Seller.cs
+++ SalesSystem/Models/Seller.cs
@@ -7,16 +7,27 @@ using System.Threading.Tasks;
 namespace SalesSystem.Models {
     public class Seller {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} é entre {2} e {1}")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "informe um capo de email válido")]
         public string Email{ get; set; }
+
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser atendido entre: {1} e {2}")]
         public double BaseSalary{ get; set; }
+
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BirthDate{ get; set; }
+
         // possui um departamento
         public Department Department{ get; set; }
         // chave estrangeira para inserir o departamento
